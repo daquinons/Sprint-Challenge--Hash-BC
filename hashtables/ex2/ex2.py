e723da8e441a7fe2d1ce06a703ded25f07907ebe
David Quiñones <daquinons@users.noreply.github.com>
2019-11-01 09:45:22 +0100

    Pass ex2 tests

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,14 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
+
+    first_flight = hash_table_retrieve(hashtable, "NONE")
+    route[0] = first_flight
+
+    for index, element in enumerate(route):
+        if index < len(route) - 1:
+            route[index + 1] = hash_table_retrieve(hashtable, route[index])
 
     return route
